3b2b4b509872643090b6419ea3d5a3b9ee72d88d
Cameron Gale <camerongale@Camerons-MacBook-Pro.local>
2016-05-09 14:35:33 -0600

    should i have done this every time i added

--- myFirstCodefile.js
+++ myFirstCodefile.js
@@ -3,3 +3,19 @@ var dm11 = "Started Today";
 var journal = [];
 
 journal.push(dm11);
+
+var fu = "first drill";
+
+journal.push(fu);
+
+var bar = "second drilll";
+
+journal.push(bar);
+
+var fubar = "thrid drill";
+
+journal.push(fubar);
+
+var testcommit = "test for my self drill";
+
+journal.push(testcommit);
